8905e91a20d969b80bb60733c5c1e8d16d88b576
GVal98 <GVal98@gmail.com>
2021-06-26 04:14:27 +0300

    Initial

---
+++ webpack.prod.js
@@ -0,0 +1,25 @@
+const { merge } = require('webpack-merge')
+const baseConfig = require('./webpack.base')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = merge(baseConfig, {
+  mode: 'production',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'main.[contenthash:4].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+})
