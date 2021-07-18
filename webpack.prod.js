1632ef34df4f047d34973ca8e17c811367fbb6e1
GVal98 <GVal98@gmail.com>
2021-07-18 19:50:26 +0300

    style: eslint

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
 const { merge } = require('webpack-merge')
-const baseConfig = require('./webpack.base')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const baseConfig = require('./webpack.base')
 
 module.exports = merge(baseConfig, {
   mode: 'production',
