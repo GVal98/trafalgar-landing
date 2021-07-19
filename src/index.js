e608dedadea117313225f8bebb58af019e53ca53
GVal98 <GVal98@gmail.com>
2021-07-19 21:35:16 +0300

    feat: slider

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import 'normalize.css';
 import './sass/main.scss';
+import Slider from './js/slider';
+
+const slider = new Slider('reviews', 'reviews__box', 'reviews__back', 'reviews__forward', 'reviews__selector');
+window.addEventListener('load', () => slider.init());
